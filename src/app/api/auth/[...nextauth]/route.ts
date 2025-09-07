7f44b9a862d5c01f94e757a938d47934ddbba5e5
masa162 <belong2jazz@gmail.com>
2025-09-07 10:49:28 +0900

    feat: Add LINE provider to NextAuth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@
 
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import LineProvider from 'next-auth/providers/line'; // LINEプロバイダーをインポート
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/lib/prisma';
 
@@ -11,6 +12,11 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+    // LINEプロバイダーを追加
+    LineProvider({
+      clientId: process.env.LINE_CHANNEL_ID ?? '',
+      clientSecret: process.env.LINE_CHANNEL_SECRET ?? '',
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
